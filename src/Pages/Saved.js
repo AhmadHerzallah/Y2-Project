74e42de01c9ff6c6c9557cb89dc10541b87de01f
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2022-01-04 22:13:22 +0200

    get items from react state and localStorage

--- src/Pages/Saved.js
+++ src/Pages/Saved.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "../Components/Card";
+import { Container, Row, Col } from "react-bootstrap";
 
-const Saved = () => {
+const Saved = ({ saved }) => {
   return (
     <div>
-      <h1>Saved</h1>
+      <Container>
+        <h1>Saved</h1>
+        <div className="row home__universities">
+          {saved.map((university) => (
+            <div className="col-md-4" key={university.id}>
+              <Card university={university} />
+            </div>
+          ))}
+        </div>
+      </Container>
     </div>
   );
 };
