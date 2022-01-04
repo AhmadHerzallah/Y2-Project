391e3a7fbd8b8e61767d6b83fb0feb7591533de0
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2022-01-05 00:05:59 +0200

    add submissionform

--- src/Components/SubmissionForm.js
+++ src/Components/SubmissionForm.js
@@ -1,9 +1,35 @@
 import React from "react";
 
 const SubmissionForm = () => {
+  // make a submission form
   return (
     <div>
-      <h1>Form يا غالي</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="hassan@hacker.com"
+          />
+        </div>
+        <br />
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Message</label>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            placeholder="ألحقنووووي"
+          ></textarea>
+        </div>
+        <br />
+        <button type="submit" className="btn btn-primary">
+          Send
+        </button>
+      </form>
     </div>
   );
 };
