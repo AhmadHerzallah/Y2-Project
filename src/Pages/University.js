68938aac3184d27d33b60f0aab2cf74d92b13ebb
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2022-01-04 22:13:08 +0200

    add save and unsave button

--- src/Pages/University.js
+++ src/Pages/University.js
@@ -1,17 +1,61 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-const University = () => {
+import { Container, Row, Col } from "react-bootstrap";
+const University = ({ saved, setSaved }) => {
   const location = useLocation();
   const navigate = useNavigate();
   const university = location.state.university;
   const { name, url, description } = university;
 
+  const save = () => {
+    setSaved((saved) => [...saved, university]);
+    window.localStorage.setItem("saved", JSON.stringify(saved));
+    navigate("/saved");
+  };
+  // unsave
+  const unsave = () => {
+    setSaved((saved) => saved.filter((university) => university.name !== name));
+    window.localStorage.setItem("saved", JSON.stringify(saved));
+    navigate("/saved");
+  };
   return (
     <div>
-      <button onClick={() => navigate(-1)}>Go Back</button>
-      <h5>{name}</h5>
-      <img src={url} alt={name} />
-      <p>{description.substr(0, 120)} ...</p>
+      <Container className="university__container">
+        <Row>
+          <Col>
+            <h1 className="university__container__title">{name}</h1>
+            <p className="university__container__description">{description}</p>
+
+            <div className="university__container__buttons">
+              <button
+                className="btn btn-primary university__button"
+                onClick={save}
+              >
+                Save
+              </button>
+              {saved.find(
+                (savedUniversity) => savedUniversity.name === university.name,
+              ) && (
+                <button
+                  className="btn btn-primary university__button"
+                  onClick={unsave}
+                >
+                  Unsave
+                </button>
+              )}
+            </div>
+          </Col>
+          <Col>
+            <div className="university__container__wrapper">
+              <img
+                className="university__container__wrapper__image"
+                src={url}
+                alt={name}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
